15a570e12e33e6a4f53f035ef61d1262478af043
Johan Quiroga <johan.c.quiroga@gmail.com>
2019-07-06 17:59:27 -0500

    feat: added state and respective types to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const App: React.FunctionComponent<object> = (): JSX.Element => {
+  const [name, setName] = useState<string>('');
+
+  function handleChange({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>): void {
+    setName(value);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <input type="text" value={name} onChange={handleChange} />
+        <p>{name}</p>
+      </header>
+    </div>
+  );
+};
 
 export default App;
